1d4e99767d291075471ce2efe5bfa9fe6e2ef466
NewLouwa <louis.aubert27000@gmail.com>
2022-03-22 06:42:38 +0100

    added svg & printsvg()

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,10 @@
 import '../Style/Header.css';
+import Linkedin from '../Assets/linkedin.svg'
+import printSvg from './PrintSvg'
 function Header() {
     return(<div className="Title"> 
      <h1>Mon profil en détail</h1>
+     {printSvg(Linkedin, 'Linkedin_Logo')}
     </div>)
 }
 
---
+++ src/Components/PrintSvg.js
@@ -0,0 +1,5 @@
+
+function printSvg(svgPath,svgAlt) {
+    return <img src={svgPath} alt={svgAlt} />
+}
+export default printSvg
